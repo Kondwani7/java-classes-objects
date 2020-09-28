c78c6e0fe954340ec5768e4f3fe88c7424f7c0bf
KONDWANI_NGULUBE <kondwanigideon.ngulube@gmail.com>
2020-09-28 13:01:34 +0200

    second commit

---
+++ classes_objects.java
@@ -0,0 +1,18 @@
+
+
+public class classes_objects {
+    static void newMethod(){
+        System.out.println("more java code practice");
+    }
+
+    static void kMethod(String name){
+      System.out.println("hey " + name);
+    }
+
+    public static void main(String[] args) {
+      newMethod();
+      newMethod();
+      newMethod();
+      kMethod("Kondwani");
+    }
+  }
